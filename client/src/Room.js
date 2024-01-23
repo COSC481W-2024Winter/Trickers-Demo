2a079866b5ba056dcf96ad83c845474ee7eba713
Jasmine Broton <jbroton@emich.edu>
2024-01-23 14:38:49 -0500

    implemented tailwind

--- client/src/Room.js
+++ client/src/Room.js
@@ -1,14 +1,16 @@
 import { useParams } from "react-router-dom";
+import './output.css'; 
 
 function Room() {
     const { roomId } = useParams();
-
     return (
         <div>
-            <div>hello from Room.js!</div>
+            <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                hello from Room.js!
+            </div>
             <div>room id: {roomId}</div>
         </div>
-    )
+    );
 }
 
 export default Room;
