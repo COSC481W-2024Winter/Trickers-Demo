fdff993a189849cf677ca5036978cca4a30d5c9e
Ellen <124838733+eyu776@users.noreply.github.com>
2024-01-22 23:52:50 -0500

    Fix: React not running on localhost
    1. Fix dependencies on package.json
    2. Remove setupProxy.js in src/ and public/
    3. Remove infinite fetching from App.js
    4. Reverted App.css to default to quickly test if application correctly loads everything by visiting index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const MyComponent = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-
-    const apiUrl = 'http://localhost:3000';
-
-    // Use fetch to make a GET request
-    fetch(apiUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        setData(responseData);
-      });
-  }, []); // The empty dependency array ensures this effect runs once after the initial render
-
+function App() {
   return (
-    <div>
-      {data ? (
-        <div> {JSON.stringify(data)}</div>
-      ) : (
-        <div>Loading...</div>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
-export default MyComponent;
+export default App;
+
+// import React, { useEffect, useState } from 'react';
+
+// const MyComponent = () => {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+
+//     const apiUrl = 'http://localhost:3000';
+
+//     // Use fetch to make a GET request
+//     fetch(apiUrl)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((responseData) => {
+//         setData(responseData);
+//       });
+//   }, []); // The empty dependency array ensures this effect runs once after the initial render
+
+//   return (
+//     <div>
+//       {data ? (
+//         <div> {JSON.stringify(data)}</div>
+//       ) : (
+//         <div>Loading...</div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default MyComponent;
